78b524c1877ace8ed695c3fcbc598e36c61f0db0
psatrawada <psatrawada@gmail.com>
2021-01-27 16:42:27 -0800

    push

--- JavaCoding/src/StringManipulations/FirstAndLastCharOfString.java
+++ JavaCoding/src/StringManipulations/FirstAndLastCharOfString.java
@@ -4,15 +4,20 @@ public class FirstAndLastCharOfString {
 	
 	public static void main(String[] args) {
 		String s = "Hello world and Hello JAVA";
-		printFirstAndLast(s);
-	}
-	public static void printFirstAndLast(String s) {
 		int n =s.length();
+		printFirstAndLast(s,n);
+		print_1standLast(s,n);
+	}
+	public static void printFirstAndLast(String s,int n) {
 		for(int i=0; i<n; i++) {
 			if(i==0 || i==n-1) {
 				System.out.println("First char: "+ s.charAt(i) + "and Last Char:" +s.charAt(i));
 			}
 		}
+	}
+	
+	public static void print_1standLast(String s, int n) {		
+		System.out.println("First And Last Char: "+ s.charAt(0)+s.charAt(n-1));		
 	}
 
 }
--- JavaCoding/src/StringManipulations/FirstLastCharOfEachWord.java
+++ JavaCoding/src/StringManipulations/FirstLastCharOfEachWord.java
@@ -3,10 +3,10 @@ package StringManipulations;
 public class FirstLastCharOfEachWord {
 	public static void main(String[] args) {
 		String s = "Hello World and Hello Java Coding";
-		printFirstLastChar(s);		
-	}
-	public static void printFirstLastChar(String s) {
 		int n = s.length();
+		printFirstLastChar(s,n);		
+	}
+	public static void printFirstLastChar(String s, int n) {		
 		for(int i=0; i<n; i++) {
 			if(i==0 || i==n-1) {
 				System.out.print(s.charAt(i));
