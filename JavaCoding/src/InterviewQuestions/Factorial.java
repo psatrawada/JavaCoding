2bcdb81047ada4dc3de4a2322dab3b7ac4e1bd30
psatrawada <psatrawada@gmail.com>
2021-01-28 16:45:44 -0800

    factorial

---
+++ JavaCoding/src/InterviewQuestions/Factorial.java
@@ -0,0 +1,15 @@
+package InterviewQuestions;
+
+public class Factorial {
+	public static void main(String[] args) {
+		System.out.println("Factorial of "+ 5 + ": "+ factorialOf(5));
+		System.out.println("Factorial of "+ 10 + ": "+ factorialOf(10));
+	}
+	public static int factorialOf(int n) {
+		if(n==1) {
+			return(1);
+		}
+		return(factorialOf(n-1)*n);
+	}
+
+}
